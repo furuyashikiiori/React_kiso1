b6e8d0c79502d8e10ab93c3d87eba308f97b54f3
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-05-24 15:29:01 +0900

    表示されるファイルの内容を変更

--- react_kiso1/src/App.jsx
+++ react_kiso1/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React+test</h1>
       <h2>これはテストだよ</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
